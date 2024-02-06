3d4fe154298eb82d099a5d9109dfc3e58f6c9429
ARIF MULANI <amnetwork049@gmail.com>
2024-02-06 10:32:21 +0530

    prio and robin(partially)

--- os_ass2/os_ass2a.cpp
+++ os_ass2/os_ass2a.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void updateTable(int i, int &strt, vector<pair<int,pii>> &v, vector<vector<int>> &table, int &tat, int &wt, int ct){
     table[i][0]=v[i].ss.ss; //jobNo
-    table[i][1]=v[i].ff; //AT 
+    table[i][1]=v[i].ff; //AT
     table[i][2]=v[i].ss.ff; //BT
     if(ct==1e9) ct=max(v[i].ff,strt)+v[i].ss.ff;
     table[i][3]=ct;// CT
@@ -25,6 +25,37 @@ void printTable(int n,vector<vector<int>> &table,int tat,int wt){
     }
     cout<<"avg tat: "<<1.0*tat/n<<endl;
     cout<<"avg wt: "<<1.0*wt/n<<endl;
+}
+
+void robin(int n, vector<pair<int,pii>> v, vector<vector<int>> table){
+    sort(v.begin(),v.end());
+    // for v => {at, {bt, jobNo}}
+    int time=v[0].ff,tat=0,wt=0;
+    queue<pair<int,pii>> q;
+    // q-> {AT,{BT, jobNo}}
+    q.push(v[0]);
+    int it=1,temp=0;
+    while(1){
+        pair<int,pii> curr=q.front();
+        q.pop();
+        time+=min(2,curr.ss.ff);
+        while(v[it].ff<=time){
+            q.push(v[it]);
+            it+=1;
+        }
+        curr.ss.ff-=2;
+        if(curr.ss.ff<=0){
+            updateTable(curr.ss.ss,temp,v,table,tat,wt,time);
+        }
+        else q.push(curr);
+        if(q.empty()){
+            if(it<n){
+                time=v[it].ff;
+                q.push(v[it]);
+            }else break;
+        }
+    }
+     printTable(n,table,tat,wt);
 }
 
 void sjf(int n, vector<pair<int,pii>> v, vector<vector<int>> table){
@@ -75,8 +106,8 @@ void  nsjf(int n, vector<pair<int,pii>> v, vector<vector<int>> table){
     int it=1;
     priority_queue<pii, vector<pii>, greater<pii>> pq;
     pq.push({v[0].ss.ff,0});
-    
-// jobNo at bt ct tat wt 
+   
+// jobNo at bt ct tat wt
     int strt=0,tat=0,wt=0;
     while(!pq.empty()){
         pii curr=pq.top();
@@ -106,5 +137,7 @@ int main(){
         v[i].ss.ss=i+1;
     }
     //fcfs(n,v,table);
-    sjf(n,v,table);
+    //sjf(n,v,table);
+    robin(n,v,table);
+   
 }
--- os_ass2/os_ass2b.cpp
+++ os_ass2/os_ass2b.cpp
@@ -6,22 +6,68 @@ using namespace std;
 
 #define pii pair<int,int>
 
-void priority(){
+void updateTable(int i, int &strt, vector<pair<pii,pii>> &v, vector<vector<int>> &table, int &tat, int &wt, int ct){
+    table[i][0]=v[i].ss.ss; //jobNo
+    table[i][1]=v[i].ff.ff; //AT
+    table[i][2]=v[i].ss.ff; //BT
+    if(ct==1e9) ct=max(v[i].ff.ff,strt)+v[i].ss.ff;
+    table[i][3]=ct;// CT
+    strt=table[i][3];
+    table[i][4]=strt-table[i][1];tat+=table[i][4];
+    table[i][5]=table[i][4]-table[i][2];wt+=table[i][5];
+    table[i][6]=v[i].ff.ss; //prio
+}
+
+void printTable(int n,vector<vector<int>> &table,int tat,int wt){
+    cout<<"job AT BT CT TAT WT PRIO\n";
+    for(auto &it:table){
+        for(int x:it) cout<<x<<" ";
+        cout<<endl;
+    }
+    cout<<"avg tat: "<<1.0*tat/n<<endl;
+    cout<<"avg wt: "<<1.0*wt/n<<endl;
+}
+
+
+void priority(int n, vector<pair<pii,pii>> v, vector<vector<int>> table){
+    sort(v.begin(),v.end());
+    // v-> at pt bt jobno
+    int it=1;
+    priority_queue<pair<int,pii>, vector<pair<int,pii>>, greater<pair<int,pii>>> pq;
+    pq.push({v[0].ff.ss,{v[0].ss.ff,0}});
+   
+    // jobNo at bt ct tat wt prio
+    int strt=0,tat=0,wt=0;
+    while(!pq.empty()){
+        pair<int,pii> curr=pq.top();
+        pq.pop();
+        updateTable(curr.ss.ss, strt, v, table, tat, wt, 1e9);
+        while(it<n){
+            if(v[it].ff.ff<=strt){
+                pq.push({v[it].ff.ss, {v[it].ss.ff, it}});
+                it+=1;
+            }
+            else break;
+        }
+    }
+    printTable(n,table,tat,wt);
 
 }
 
 int main(){
-    //priority + robin round. must work in either case.
     //given -> AT BT pri
     int n;
     cin>>n;
     cout << fixed << setprecision(5);
     // at pt bt jobno.
     vector<pair<pii,pii>> v(n);
-    // at bt ct tat wt
-    vector<vector<int>> table(n,vector<int>(6));
+    // jbNO at pt bt ct tat wt
+    vector<vector<int>> table(n,vector<int>(7));
+   
     for(int i=0;i<n;i++){
         cin>>v[i].ff.ff>>v[i].ff.ss>>v[i].ss.ff;
         v[i].ss.ss=i+1;
     }
+    priority(n,v,table);
+   
 }
